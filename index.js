45c1f70965bbcc0d7725640a150130f72cc50e96
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-01 13:15:56 +0300

    commit tree c928f971020bf770cd4cf1f55c4a9611de2cbac9

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  
+
+  function whisper (string) {
+    return (string.toLowerCase( ));
+    console.log(whisper());
+  }
+  function logShout (string) {
+    console.log(string.toUpperCase( ));}
+    
+    function logWhisper (string) {
+        console.log(string.toLowerCase( ));}
+
+function sayHiToHeadphonedRoommate (string) {
+    var cantUnswer = "I can't hear you!";
+    var yesUnswer = "YES INDEED!";
+    var lovUnswer = "I would love to.";
+       if (string.toLowerCase(string) === string) {
+              return cantUnswer;
+            }
+       else if (string.toUpperCase(string) === string) {
+              return yesUnswer;
+            }
+       else if ("I would love to." === string) {
+              return lovUnswer
+            }
+        }
